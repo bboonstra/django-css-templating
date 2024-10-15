52b7f9c55bd09e52d0a3de47a7e4faa11654ac3a
Ben Boonstra <bboonstra26@gmail.com>
2024-10-15 13:44:11 -0500

    Remove debug message

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@ import { activateLanguageDetector } from './languageDetector';
 
 export function activate(context: vscode.ExtensionContext) {
 	activateLanguageDetector(context);
-	vscode.window.showInformationMessage('Debug: Django CSS Templating loaded properly.');
 }
 
 export function deactivate() {}
